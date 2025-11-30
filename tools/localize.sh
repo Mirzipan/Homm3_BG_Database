278418a6c5d13abd79f92d8c5e782a1a91903f86
qwrtln <qwrtln@users.noreply.github.com>
2025-11-30 12:18:34 +0100

    Introduce separate instances for localized versions (#66)
    * Initial configuration
    
    * Localize navigation
    
    * Localize navigation
    
    * Set default nav for EN
    
    * Add localized sorted navigation
    
    * Add localization workflow
    
    * Add French to scripts
    
    * Add flag icons
    
    * Move localized navigation to translations
    
    * Fix workflow
    
    * Restore po4a.cfg state
    
    * Remove po4a step
    
    * Fix typo
    
    * Remove unnecessary trigger

---
+++ tools/localize.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+LANGUAGE=$1
+
+
+git restore docs/
+
+find docs -name "*.pl.md" -delete
+find docs -name "*.es.md" -delete
+find docs -name "*.fr.md" -delete
+
+if [[ $LANGUAGE == "en" ]]; then
+  git restore mkdocs.yml
+  exit
+fi
+
+po4a --no-update po4a.cfg --target-lang "${LANGUAGE}"
+
+find docs/ -name "*.$LANGUAGE.md"| while IFS= read -r file; do
+  target="${file::-5}md"
+  echo "Moving file: $file -> $target"
+  mv "$file" "$target"
+done
+
+cp "translations/navigation/$LANGUAGE.yml" docs/.nav.yml
+
+sed -i -e "s/language: en/language: $LANGUAGE/" \
+    -e "s/alternate.*-round/alternate: $LANGUAGE-round/" \
+  mkdocs.yml
